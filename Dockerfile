7a7f7a34a4f1203d20cbf472456cab2f20a96302
Chanwit Kaewkasi <chanwit@gmail.com>
2016-01-31 11:37:22 -0500

    init

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM springboot:onbuild
+
---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public @ResponseBody Map<String, String> home() {
+		return new HashMap<String, String>(){{
+			put("hello", "world");
+		}};
+	}
+
+        public static void main(String[] args) {
+        	SpringApplication.run(Application.class, args);
+        }
+}
