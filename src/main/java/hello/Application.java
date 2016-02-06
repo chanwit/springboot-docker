7bcd30826288b7cc380673f902575c2dbe0aadba
Chanwit Kaewkasi <chanwit@gmail.com>
2016-02-06 01:55:52 -0500

    change

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -18,7 +18,7 @@ public class Application {
 	@RequestMapping("/")
 	public @ResponseBody Map<String, String> home() {
 		return new HashMap<String, String>(){{
-			put("hello", "docker world");
+			put("edit", "docker world");
 		}};
 	}
 
